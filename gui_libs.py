18ccdd8af3ce6acf1bd8d78282e95ba5042ea3b7
Kezif <kezif6262@gmail.com>
2021-04-25 20:23:00 +0300

    Backend for plot displaing changed from pyqt4 -> wx

--- gui_libs.py
+++ gui_libs.py
@@ -5,15 +5,16 @@ from traits.trait_types import File, Button, Range, Int, Float, String
 from traits.etsconfig.api import ETSConfig
 from pyface.api import FileDialog, OK
 from traitsui.api import View, Item, HGroup, Group, VGroup
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+import wx
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.figure import Figure
 from traits.api import Instance
-from traitsui.qt4.editor import Editor
-from traitsui.qt4.basic_editor_factory import BasicEditorFactory
+from traitsui.wx.editor import Editor
+from traitsui.wx.basic_editor_factory import BasicEditorFactory
 import os
-matplotlib.use('Qt4Agg')
+matplotlib.use('WXAgg')
 ROOT_DIR = getcwd() + '\\'
-ETSConfig.toolkit = 'qt4'
+#ETSConfig.toolkit = 'qt4'
     
 class _MPLFigureEditor(Editor):  # 2 classes for ploting
     scrollable = True
@@ -28,8 +29,11 @@ class _MPLFigureEditor(Editor):  # 2 classes for ploting
     def _create_canvas(self, parent):
         """ Create the MPL canvas. """
         # matplotlib commands to create a canvas
-        mpl_canvas = FigureCanvas(self.value)
-        return mpl_canvas
+        #mpl_canvas = FigureCanvas(self.value)
+        #return mpl_canvas
+        panel = wx.Panel(parent, -1, style=wx.CLIP_CHILDREN)
+        mpl_control = FigureCanvas(panel, -1, self.value) 
+        return panel
 
 
 class MPLFigureEditor(BasicEditorFactory):
